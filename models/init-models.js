30d1fa03ba53669acf5d0630147cb2ac9798c2c7
ayung21 <ayungyung20@gmail.com>
2025-09-18 14:26:56 +0700

    Refactor init-models.js to include missing model initializations for access, billingsave, cabang, history_units, member, produk, promo, role, transaksi, transaksi_detail, and units

--- models/init-models.js
+++ models/init-models.js
@@ -1,11 +1,44 @@
 var DataTypes = require("sequelize").DataTypes;
+var _access = require("./access");
+var _billingsave = require("./billingsave");
+var _cabang = require("./cabang");
+var _history_units = require("./history_units");
+var _member = require("./member");
+var _produk = require("./produk");
+var _promo = require("./promo");
+var _role = require("./role");
+var _transaksi = require("./transaksi");
+var _transaksi_detail = require("./transaksi_detail");
+var _units = require("./units");
 var _users = require("./users");
 
 function initModels(sequelize) {
+  var access = _access(sequelize, DataTypes);
+  var billingsave = _billingsave(sequelize, DataTypes);
+  var cabang = _cabang(sequelize, DataTypes);
+  var history_units = _history_units(sequelize, DataTypes);
+  var member = _member(sequelize, DataTypes);
+  var produk = _produk(sequelize, DataTypes);
+  var promo = _promo(sequelize, DataTypes);
+  var role = _role(sequelize, DataTypes);
+  var transaksi = _transaksi(sequelize, DataTypes);
+  var transaksi_detail = _transaksi_detail(sequelize, DataTypes);
+  var units = _units(sequelize, DataTypes);
   var users = _users(sequelize, DataTypes);
 
 
   return {
+    access,
+    billingsave,
+    cabang,
+    history_units,
+    member,
+    produk,
+    promo,
+    role,
+    transaksi,
+    transaksi_detail,
+    units,
     users,
   };
 }
