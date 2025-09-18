f5da372f4810efd72c0aef939ecf5892f778040e
ayung21 <ayungyung20@gmail.com>
2025-09-18 16:29:27 +0700

    Refactor produk model: rename 'nama' to 'name' and adjust stok field definition

--- models/produk.js
+++ models/produk.js
@@ -12,13 +12,21 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       comment: "1. makanan\n2. minuman"
     },
-    nama: {
-      type: DataTypes.STRING(50),
-      allowNull: false
-    },
     stok: {
       type: DataTypes.INTEGER,
       allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    harga_beli: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    harga_jual: {
+      type: DataTypes.INTEGER,
+      allowNull: true
     },
     cabang: {
       type: DataTypes.INTEGER,
