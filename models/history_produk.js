84a23ece5bf86a14dab925191a44938499096ff5
ayung21 <ayungyung20@gmail.com>
2025-09-19 08:40:21 +0700

    Add history_produk model definition with fields and configurations

---
+++ models/history_produk.js
@@ -0,0 +1,60 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('history_produk', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    produkid: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    type: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    stok: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    harga_beli: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    harga_jual: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    created_by: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    tableName: 'history_produk',
+    timestamps: true,
+    createdAt: 'createdAt',
+    updatedAt: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
