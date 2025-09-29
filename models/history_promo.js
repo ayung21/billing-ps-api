0668c62f4e377f6809f50f7ad0e95b2d96432c69
ayung21 <ayungyung20@gmail.com>
2025-09-29 05:34:28 +0200

    Add history_promo model definition with Sequelize

---
+++ models/history_promo.js
@@ -0,0 +1,60 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('history_promo', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    unitid: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    cabangid: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    discount_percent: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    discount_nominal: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    hours: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    created_by: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    tableName: 'history_promo',
+    timestamps: true,
+    createdAt: 'createdAt',
+    updatedAt: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
