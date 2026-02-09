6b101a3ff4fb07313d63d201efe26a93da986f07
ayung21 <ayungyung20@gmail.com>
2026-02-09 07:58:39 +0700

    feat(database): Add MySQL dump for billingps database with table structures and initial data

--- dump-billingps-09-02-2026-202602090757.sql
+++ dump-billingps-09-02-2026-202602090757.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.43, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.44, for Linux (x86_64)
 --
 -- Host: localhost    Database: billingps
 -- ------------------------------------------------------
--- Server version	8.0.43-0ubuntu0.24.04.1
+-- Server version	8.0.44-0ubuntu0.24.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,7 +27,7 @@ CREATE TABLE `access` (
   `userid` int NOT NULL,
   `cabangid` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,6 +36,7 @@ CREATE TABLE `access` (
 
 LOCK TABLES `access` WRITE;
 /*!40000 ALTER TABLE `access` DISABLE KEYS */;
+INSERT INTO `access` VALUES (1,1,1);
 /*!40000 ALTER TABLE `access` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,6 +80,10 @@ DROP TABLE IF EXISTS `brandtv`;
 CREATE TABLE `brandtv` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(25) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `codetvid` int DEFAULT NULL,
+  `cabangid` int DEFAULT NULL,
+  `tv_id` text COLLATE utf8mb4_general_ci,
+  `ip_address` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -126,9 +131,10 @@ DROP TABLE IF EXISTS `codetv`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `codetv` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `brandtvid` int DEFAULT NULL,
+  `name` varchar(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `code` int DEFAULT NULL,
   `desc` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `command` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -151,12 +157,16 @@ DROP TABLE IF EXISTS `history_produk`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `history_produk` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `token` text COLLATE utf8mb4_general_ci,
   `produkid` int DEFAULT NULL,
   `type` int DEFAULT NULL,
   `name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `stok` int DEFAULT NULL,
   `harga_beli` int DEFAULT NULL,
   `harga_jual` int DEFAULT NULL,
+  `cabangid` int DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `desc` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `created_by` int DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -169,8 +179,42 @@ CREATE TABLE `history_produk` (
 
 LOCK TABLES `history_produk` WRITE;
 /*!40000 ALTER TABLE `history_produk` DISABLE KEYS */;
-INSERT INTO `history_produk` VALUES (1,NULL,1,'mie goreng',12,1000,4000,'2025-09-19 08:46:30',1),(2,NULL,1,'mie goreng indomie',12,1000,4000,'2025-09-19 08:47:06',1),(3,NULL,1,'mie goreng indomie',5,1000,4000,'2025-09-19 08:47:43',1),(4,1,1,'mie goreng indomie',5,1000,4000,'2025-09-19 08:48:12',1);
+INSERT INTO `history_produk` VALUES (1,NULL,NULL,1,'mie goreng',12,1000,4000,NULL,NULL,NULL,'2025-09-19 08:46:30',1),(2,NULL,NULL,1,'mie goreng indomie',12,1000,4000,NULL,NULL,NULL,'2025-09-19 08:47:06',1),(3,NULL,NULL,1,'mie goreng indomie',5,1000,4000,NULL,NULL,NULL,'2025-09-19 08:47:43',1),(4,NULL,1,1,'mie goreng indomie',5,1000,4000,NULL,NULL,NULL,'2025-09-19 08:48:12',1);
 /*!40000 ALTER TABLE `history_produk` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `history_promo`
+--
+
+DROP TABLE IF EXISTS `history_promo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `history_promo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `token` text COLLATE utf8mb4_general_ci,
+  `promoid` int DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `unitid` int DEFAULT NULL,
+  `cabangid` int DEFAULT NULL,
+  `discount_percent` int DEFAULT NULL,
+  `discount_nominal` int DEFAULT NULL,
+  `hours` int DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `desc` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created_by` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `history_promo`
+--
+
+LOCK TABLES `history_promo` WRITE;
+/*!40000 ALTER TABLE `history_promo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `history_promo` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -242,8 +286,10 @@ DROP TABLE IF EXISTS `produk`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `produk` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `token` text COLLATE utf8mb4_general_ci,
   `type` int NOT NULL COMMENT '1. makanan\n2. minuman',
   `stok` int NOT NULL,
+  `warning_level` int DEFAULT '5',
   `name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `harga_beli` int DEFAULT NULL,
   `harga_jual` int DEFAULT NULL,
@@ -263,7 +309,7 @@ CREATE TABLE `produk` (
 
 LOCK TABLES `produk` WRITE;
 /*!40000 ALTER TABLE `produk` DISABLE KEYS */;
-INSERT INTO `produk` VALUES (1,1,5,'mie goreng indomie',1000,4000,1,1,'2025-09-18 16:26:09',1,'2025-09-19 08:47:43',1);
+INSERT INTO `produk` VALUES (1,NULL,1,5,5,'mie goreng indomie',1000,4000,1,1,'2025-09-18 16:26:09',1,'2025-09-19 08:47:43',1);
 /*!40000 ALTER TABLE `produk` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -276,7 +322,10 @@ DROP TABLE IF EXISTS `promo`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `promo` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `token` text COLLATE utf8mb4_general_ci,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `unitid` int DEFAULT NULL,
+  `cabangid` int DEFAULT NULL,
   `discount_percent` int DEFAULT NULL,
   `discount_nominal` int DEFAULT NULL,
   `hours` int DEFAULT NULL,
@@ -285,7 +334,6 @@ CREATE TABLE `promo` (
   `created_by` int DEFAULT NULL,
   `updatedAt` datetime NOT NULL,
   `updated_by` int DEFAULT NULL,
-  `name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -296,7 +344,7 @@ CREATE TABLE `promo` (
 
 LOCK TABLES `promo` WRITE;
 /*!40000 ALTER TABLE `promo` DISABLE KEYS */;
-INSERT INTO `promo` VALUES (1,2,10,NULL,3,1,'2025-09-18 15:20:15',1,'2025-09-18 15:21:17',1,'weekend');
+INSERT INTO `promo` VALUES (1,NULL,'weekend',2,NULL,10,NULL,3,1,'2025-09-18 15:20:15',1,'2025-09-18 15:21:17',1);
 /*!40000 ALTER TABLE `promo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -313,7 +361,7 @@ CREATE TABLE `role` (
   `role` int DEFAULT NULL,
   `status` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -322,6 +370,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,1,1,1),(2,1,2,1),(3,1,3,1),(4,1,4,1),(5,1,5,1),(6,1,6,1),(7,1,7,1),(8,1,8,1),(9,1,9,1),(10,1,10,1),(11,1,11,1),(12,1,12,1),(13,1,13,1),(14,1,14,1),(15,1,15,1),(16,1,16,1);
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -337,6 +386,7 @@ CREATE TABLE `transaksi` (
   `memberid` int DEFAULT NULL,
   `customer` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `telepon` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `cabangid` int DEFAULT NULL,
   `grandtotal` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `status` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '0. selesai\n1. main',
   `createdAt` datetime NOT NULL,
@@ -365,12 +415,14 @@ DROP TABLE IF EXISTS `transaksi_detail`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `transaksi_detail` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `type` int DEFAULT NULL,
   `code` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `promoid` int DEFAULT NULL,
-  `produk` int DEFAULT NULL,
-  `unitid` int DEFAULT NULL,
+  `promo_token` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `produk_token` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `unit_token` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `hours` int DEFAULT NULL,
+  `qty` int DEFAULT NULL,
   `harga` int DEFAULT NULL,
   `status` int DEFAULT NULL COMMENT '0. non-active\n1. active',
   `createdAt` datetime NOT NULL,
@@ -399,15 +451,20 @@ DROP TABLE IF EXISTS `units`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `units` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `token` text COLLATE utf8mb4_general_ci,
   `name` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `brandtvid` int DEFAULT NULL,
   `cabangid` int DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `description` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `status` int NOT NULL DEFAULT '1' COMMENT '0. non-active\n1. active\n2. maintenance',
   `createdAt` datetime NOT NULL,
   `created_by` int DEFAULT NULL,
   `updatedAt` datetime NOT NULL,
   `updated_by` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `brandtvid` (`brandtvid`) USING BTREE,
+  KEY `cabangid` (`cabangid`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -417,7 +474,7 @@ CREATE TABLE `units` (
 
 LOCK TABLES `units` WRITE;
 /*!40000 ALTER TABLE `units` DISABLE KEYS */;
-INSERT INTO `units` VALUES (1,'ps 5 nomer 4',NULL,1,0,'2025-09-18 13:34:36',1,'2025-09-18 14:30:19',1),(2,'ps 5 nomer 3',NULL,1,1,'2025-09-18 14:30:00',1,'2025-09-18 14:30:00',1);
+INSERT INTO `units` VALUES (1,NULL,'ps 5 nomer 4',NULL,1,NULL,NULL,0,'2025-09-18 13:34:36',1,'2025-09-18 14:30:19',1),(2,NULL,'ps 5 nomer 3',NULL,1,NULL,NULL,1,'2025-09-18 14:30:00',1,'2025-09-18 14:30:00',1);
 /*!40000 ALTER TABLE `units` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -434,7 +491,7 @@ CREATE TABLE `users` (
   `username` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `password` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `status` int DEFAULT NULL COMMENT '0. non - active\n1. active',
-  `activ_period` datetime DEFAULT NULL,
+  `active_period` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -445,7 +502,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'ayungyung20@gmail.com','ayung','$2b$10$eBF.vSnoNNk3oC7pRRcMDumtMovcdnCJ3TJKzfqqWE9fZI7jA55ra',1,'2025-09-18 09:11:06');
+INSERT INTO `users` VALUES (1,'ayungyung20@gmail.com','ayung','$2b$10$h6aLvPYVVJNsLw3t10dqDOnlSnQnBza2MTcysm/ttXVgePObsc.cu',1,'2026-02-08 14:30:24');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -462,4 +519,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-19 14:57:03
+-- Dump completed on 2026-02-09  7:57:00
